c0ded48b646664d7ce5eed1dd51502094438d62e
Larysse Savanna Izidio da Silva <laryssesavanna@lcc.ufrn.br>
2019-12-16 02:40:26 -0300

    Creating project

---
+++ catalogo-discos/src/components/Menu.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Aqui fica o menu</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+};
+</script>
---
+++ catalogo-discos/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Menu/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Menu from '@/components/Menu.vue';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    Menu,
+  },
+};
+</script>
